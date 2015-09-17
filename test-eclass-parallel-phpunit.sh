78aec59b24af2cf6d75a62c233fa68fbdffc0236
Joey Arnold <jandres@ualberta.ca>
2015-09-17 13:29:08 -0600

    MAIN-8-2: Fixed the output of result.

--- test-eclass-parallel-phpunit.sh
+++ test-eclass-parallel-phpunit.sh
@@ -6,5 +6,6 @@ container_result_file="$3"
 
 
 test_output="$(docker exec ${container} /home/lmsadmin/run_phpunit_testsuites_subset.sh $testsuites)"
-echo $test_output >> $container_result_file 2>&1  # Output for the file.
-echo $test_output  # Output for terminal.
+test_output="${test_output}\n"
+echo -e $test_output >> $container_result_file 2>&1  # Output for the file.
+echo -e $test_output  # Output for terminal.
